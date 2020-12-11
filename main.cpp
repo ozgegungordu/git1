2045a582dbdc74585a1604bbc4b8c78835abeb5c
Ozge Gungordu <ozge.gungordu@dormakaba-external.com>
2020-12-11 15:59:48 +0100

    Hello ben ozge

--- main.cpp
+++ main.cpp
@@ -3,5 +3,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	
+	int a,b,c;
+	printf("Bir sayi giriniz : ");
+	scanf("%d",&a);
+	printf("ikinci sayiyi giriniz : ");
+	scanf("%d",&b);
+	c = a+b ;
+	printf("%d + %d = %d",a,b,c);
+	
 	return 0;
 }
